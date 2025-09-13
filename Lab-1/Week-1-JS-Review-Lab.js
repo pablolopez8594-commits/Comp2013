90f504ed43807a6e418bb7cf35596259dcf50c9c
Pablo Lopez <pablo.lopez.8594@gmail.com>
2025-09-12 22:26:58 -0400

    lAB ONE DONEEEE

--- Lab-1/Week-1-JS-Review-Lab.js
+++ Lab-1/Week-1-JS-Review-Lab.js
@@ -477,14 +477,18 @@ const listings = [
  * from the array above
  */
 //WRITE YOUR CODE BELOW
-
+let listing0 = listings[0]
 /**
  * Task-2: create an object named listing1 using the spread operator and add 2 new properties,
  * isSold which is a boolean set to false,
  * and currentOwner which is a string with the value of Jane Doe
  */
 //WRITE YOUR CODE BELOW
-
+const listing1 = {
+  ...listing0,
+  isSold : true,
+  currentOwner: "Jane Doe",
+};
 /**
  * Task-3: print to console a text using template literals that contains the name of the owner,
  * and their current address for listing1.
@@ -492,7 +496,7 @@ const listings = [
  * "This house at (address) is owned by (currentOwner)"
  */
 //WRITE YOUR CODE BELOW
-
+console.log(`This house at ${listing1.address} is owned by ${listing1.currentOwner}`)
 /**
  * NOTE: THIS TASK IS TRICKY!
  * Task-4: Create a function to calculate the realtor fees.
@@ -507,14 +511,38 @@ const listings = [
  * USE TERNARIES INSTEAD OF IF/ELSE STATEMENTS!
  */
 //WRITE YOUR CODE BELOW
+function removeChar(string){
+  let numericValue = Number(string.replace("$","").replaceAll(",",""));
+  return numericValue;
+}
+function realtorFees(listing){
 
+  let fee = removeChar(listing.price) <= 450000 ? removeChar(listing.price) * 0.025 : removeChar(listing.price) * 0.02;
+  return fee;
+// if(numericValue<=450000){
+// let fee=numericValue*0.025;
+// return fee;
+// }
+// else{
+//   let fee = numericValue*0.02;
+//   return fee;
+// }
+
+}
+console.log(realtorFees(listing1))
 /**
  * Task-5: Sort the listing array ascendingly in a new variable called listingAscendingly
  */
 //WRITE YOUR CODE BELOW
-
+const listingAscendingly = listings  
+.slice()
+  .sort((a, b) => removeChar(a.price) - removeChar(b.price));
+  console.log(listingAscendingly);
 /**
  * Task-6: filter all the listings by bulitIn propertity for all houses built on or after 1990.
  * Name the variable newBuiltListings
+ * 
  */
 //WRITE YOUR CODE BELOW
+const newBuiltListings = listings.filter((a) => removeChar(a.propertySummary.builtIn) >= 1990);
+console.log(newBuiltListings);
